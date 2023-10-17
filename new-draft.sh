9da4e68869c6c38c28bd47aa4d10186b6b52f32a
Peter Williams <peter@newton.cx>
2023-10-16 21:05:45 -0400

    new-draft.sh: new helper, we'll give it a try

---
+++ new-draft.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#
+# Quickie script to stub a draft
+
+set -e
+cd $(dirname $0)
+slug="$1"
+
+if [ x"$slug" = x ] ; then
+    echo "usage: $0 new-post-filename-slug"
+    exit 0
+fi
+
+if [[ "$slug" =~ \.md$ ]] ; then  # note, shell quoting not needed!
+    :
+else
+    slug="$slug.md"
+fi
+
+mkdir -p drafts
+
+path="drafts/$slug"
+
+cat <<EOF >"$path"
++++
+date = @DATE@
+title = ""
++++
+
+<!-- more -->
+
+EOF
+
+exec hidpi-chromium-launcher.sh code --new-window "$path"
